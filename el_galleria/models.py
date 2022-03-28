04a28af907168a73ea7581a98143800783053ff3
kennjr <ken.juniour@student.moringaschool.com>
2022-03-28 22:50:15 +0300

    Created the location model with all the columns it needs

--- el_galleria/models.py
+++ el_galleria/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=199, blank=True)
+
+    def __str__(self):
+        return f'{self.name} ({self.address})'
+
+    class Meta:
+        # The line below will give the tbl the name specified in the "'
+        db_table = "location"
+
