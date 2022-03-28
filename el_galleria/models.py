30371a15e663b99360fbcfedff706320617818fa
kennjr <ken.juniour@student.moringaschool.com>
2022-03-28 22:54:24 +0300

    Created the category model with 2 columns

--- el_galleria/models.py
+++ el_galleria/models.py
@@ -12,3 +12,16 @@ class Location(models.Model):
         # The line below will give the tbl the name specified in the "'
         db_table = "location"
 
+
+class Category(models.Model):
+    category_str = models.CharField(max_length=59)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return f'{self.category_str} ({self.slug})'
+
+    class Meta:
+        # The line below will give the tbl the name specified in the "'
+        db_table = "category"
+
+
