b3f806aa30a170d13be553e7d18f8c8d3aa527be
kennjr <ken.juniour@student.moringaschool.com>
2022-03-28 23:56:03 +0300

    Created the galleria model with some of the columns needed by the tbl

--- el_galleria/models.py
+++ el_galleria/models.py
@@ -25,3 +25,18 @@ class Category(models.Model):
         db_table = "category"
 
 
+class Galleria(models.Model):
+    img_url = models.ImageField(upload_to='galleria_imgs')
+    description = models.TextField()
+    timestamp = models.CharField(max_length=101)
+    caption = models.CharField(max_length=88)
+    loc = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True)
+    cat = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f'{self.caption} at ({self.loc})'
+
+    class Meta:
+        # The line below will give the tbl the name specified in the "'
+        db_table = "galleria"
+
