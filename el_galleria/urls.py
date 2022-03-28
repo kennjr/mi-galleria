fe511d555d5f6628dd3abfbee225dcab8cf3c838
kennjr <ken.juniour@student.moringaschool.com>
2022-03-28 09:35:06 +0300

    Added the category url and view fun.

--- el_galleria/urls.py
+++ el_galleria/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from el_galleria import views
 
 urlpatterns = [
-    path('', views.index, name="home")
+    path('', views.index, name="home"),
+    path('category/<str:selected_category>/', views.category, name="category"),
+    path('search/<str:search_str>/', views.search, name="search")
+
 ]
--- el_galleria/views.py
+++ el_galleria/views.py
@@ -3,4 +3,18 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'el_galleria/index.html')
+    return render(request, 'el_galleria/index.html', {'category': 'all'})
+
+
+def search(request, search_str):
+    if request.method == "POST":
+        input_search_str = request.POST.get('search_str', None)
+        return render(request, 'el_galleria/search.html', {'category': input_search_str})
+    else:
+        # todo Make search request to the db, for all imgs that match the category
+        return render(request, 'el_galleria/search.html', {'category': search_str})
+
+
+def category(request, selected_category):
+    return render(request, 'el_galleria/index.html', {'category': selected_category})
+
