1c276627e15dc29a21c79f3333184b2fe74c4de4
kennjr <ken.juniour@student.moringaschool.com>
2022-03-29 08:16:48 +0300

    Sorted out a few naming errors for the admin classes

--- el_galleria/admin.py
+++ el_galleria/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+# category_str
 from .models import Category, Location, Galleria
 # Register your models here.
 
@@ -8,9 +8,9 @@ from .models import Category, Location, Galleria
 class GalleriaAdmin(admin.ModelAdmin):
     # The line below will allow us to display the meetup obj items in a list with the specified columns
     # the columnnames in the models are what we've used in the strs
-    list_display = ('caption', 'loc', 'timestamp')
+    list_display = ('caption', 'location', 'timestamp')
     # The line below will allow us to have filter opts for our list of entries that appear in the admin dash
-    list_filter = ('timestamp', "loc", "category")
+    list_filter = ('timestamp', "location", "category")
     # The line below will pre-populate the slug field based on the title we enter,
     #  the key is the param that we'd like to pre-populate and the tuple has the entries that'll be concat'ed to create the key
     # prepopulated_fields = {'slug': ('title',)}
