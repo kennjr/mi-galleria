c45e8cff5ddfce65d5521b4814a939f6cbf9abba
kennjr <ken.juniour@student.moringaschool.com>
2022-03-29 10:03:57 +0300

    Wrote the tests for the category model

--- el_galleria/tests.py
+++ el_galleria/tests.py
@@ -8,9 +8,9 @@ django.setup()
 
 from .models import Location, Category, Galleria
 
-# Create your tests here.
 
-class LocationTestClass(TestCase):
+# Create your tests here.
+class LocationTestsClass(TestCase):
     # set up method
     def setUp(self):
         self.location = Location.objects.create(name="Kilimani", address="5 Montie St")
@@ -21,12 +21,12 @@ class LocationTestClass(TestCase):
         self.assertEqual(self.location, location)
 
     # testing save method
-    def test_save_method(self):
+    def test_save_location_method(self):
         self.location.save_location()
         location = Location.objects.all()
         self.assertTrue(len(location) > 0)
 
-    def test_delete_method(self):
+    def test_delete_location_method(self):
         self.location.save_location()
         original_len = Location.objects.all()
         print(f'the locations are{len(original_len)}')
@@ -34,4 +34,31 @@ class LocationTestClass(TestCase):
         locations = Location.objects.all()
         print(f'the locations are{len(locations)}')
         self.assertTrue((len(locations)) == (len(original_len) - 1))
+
+
+class CategoryTestsClass(TestCase):
+    # set up method
+    def setUp(self):
+        self.category = Category.objects.create(category_str="traveller", slug="travellerr")
+
+    # testing instance
+    def test_instance(self):
+        category = self.category
+        self.assertEqual(self.category, category)
+
+    # testing save method
+    def test_save_category_method(self):
+        self.category.save_category()
+        location = Category.objects.all()
+        self.assertTrue(len(location) > 0)
+
+    def test_delete_category_method(self):
+        self.category.save_category()
+        original_len = Category.objects.all()
+        print(f'the categorys are{len(original_len)}')
+        Category.delete_category(self.category.id)
+        categorys = Category.objects.all()
+        print(f'the categorys are{len(categorys)}')
+        self.assertTrue((len(categorys)) == (len(original_len) - 1))
+
 
