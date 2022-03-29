1890a52858ad8dffa1c3d50f3a252006ec318965
kennjr <ken.juniour@student.moringaschool.com>
2022-03-29 10:29:25 +0300

    Wrote the tests for the galleria model

--- el_galleria/tests.py
+++ el_galleria/tests.py
@@ -62,3 +62,57 @@ class CategoryTestsClass(TestCase):
         self.assertTrue((len(categorys)) == (len(original_len) - 1))
 
 
+class GalleriaTestsClass(TestCase):
+    # set up method
+    def setUp(self):
+        self.test_location = Location.objects.create(name='Kileleshwa', address="1 Kieni Rd")
+        # self.test_location.save_location()
+
+        # creating a new category
+        self.test_category = Category.objects.create(category_str='test category', slug="test-category")
+        # self.test_category.save_category()
+
+        self.gallery_img = Galleria(description='The mountains', img_url='galleria_imgs/hobbies_7.jpg',
+                                    location=self.test_location, category=self.test_category,
+                                    timestamp="test_time", caption="a test caption")
+
+    # testing instance
+    def test_instance(self):
+        img = self.gallery_img
+        self.assertEqual(self.gallery_img, img)
+
+    # testing save method
+    def test_save_img_method(self):
+        original_len = Galleria.get_all_images()
+        print(f'original len {len(original_len)}')
+        self.gallery_img.save_image()
+        new_len = Galleria.get_all_images()
+        print(f'new len {len(new_len)}')
+        self.assertTrue(len(new_len) > len(original_len))
+
+    def test_delete_img_method(self):
+        self.gallery_img.save_image()
+        original_len = Galleria.objects.all()
+        print(f'the categorys are{len(original_len)}')
+        Galleria.delete_image(self.gallery_img.id)
+        new_len = Galleria.objects.all()
+        print(f'the categorys are{len(new_len)}')
+        self.assertTrue((len(new_len)) == (len(original_len) - 1))
+
+    def test_get_img_by_id_method(self):
+        self.gallery_img.save_image()
+        req_result = Galleria.get_image_by_id(self.gallery_img.id)
+        self.assertTrue(req_result is not None)
+
+    def test_search_gallery_method(self):
+        self.gallery_img.save_image()
+        search_results = Galleria.search_gallery_by_category("test category")
+        print(f'The list length {len(search_results)}')
+        self.assertTrue(search_results != [])
+
+    def test_filter_gallery_list_method(self):
+        self.gallery_img.save_image()
+        filter_results = Galleria.filter_by_location("Kileleshwa")
+        print(f'The list length {len(filter_results)}')
+        self.assertTrue(filter_results != [])
+
